a9dadab9b6bfec74250f1bdb1e2e28f48d11bf65
Piyush Kr Dey <deypiyushkumar2004@gmail.com>
2025-02-17 23:02:24 +0530

    Update ml.py

--- ml.py
+++ ml.py
@@ -48,7 +48,7 @@ model = models.Sequential([
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
     layers.Conv2D(128, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
+    layers.MaxPooling2D((2, 2)), 
     layers.Conv2D(128, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
     layers.Flatten(),
